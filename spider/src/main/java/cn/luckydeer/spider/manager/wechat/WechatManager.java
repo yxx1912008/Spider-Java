6542a2ce0eafdc8bae219e4a910fce28869dc92e
yuanxx <824264439@qq.com>
2019-01-10 18:08:02 +0800

    添加微信机器人管理类

---
+++ spider/src/main/java/cn/luckydeer/spider/manager/wechat/WechatManager.java
@@ -0,0 +1,14 @@
+package cn.luckydeer.spider.manager.wechat;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 
+ * 微信机器人管理
+ * @author yuanxx
+ * @version $Id: WechatManager.java, v 0.1 2019年1月10日 下午6:07:17 yuanxx Exp $
+ */
+@Service(value = "wechatManager")
+public class WechatManager {
+
+}
