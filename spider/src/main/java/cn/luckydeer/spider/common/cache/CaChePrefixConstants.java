c626394ff7a4743322cf395ecaa78d39fa16c4c0
Dung Cry <824264439@qq.com>
2019-01-28 16:59:12 +0800

    增加缓存前缀

---
+++ spider/src/main/java/cn/luckydeer/spider/common/cache/CaChePrefixConstants.java
@@ -0,0 +1,17 @@
+package cn.luckydeer.spider.common.cache;
+
+/**
+ * 
+ * 缓存前缀
+ * @author yuanxx
+ * @version $Id: CaChePrefixConstants.java, v 0.1 2018年10月8日 上午9:44:10 yuanxx Exp $
+ */
+public interface CaChePrefixConstants {
+
+    /**购物猫 商品详情 图片缓存*/
+    String GOOD_IMG_CACHE             = "GOOD_IMG_CACHE";
+
+    /** 微信公众号  ACCESS_TOKEN 缓存前缀 */
+    String WEIXIN_PUBLIC_ACCESS_TOKEN = "WEIXIN_PUBLIC_ACCESS_TOKEN_";
+
+}
