c92b77cf4a38373a71a4c7d9f749c16281e4f72e
Dung Cry <824264439@qq.com>
2019-01-28 15:21:45 +0800

    修改自动扫包规则

--- spider/src/main/java/cn/luckydeer/spider/SpiderApplication.java
+++ spider/src/main/java/cn/luckydeer/spider/SpiderApplication.java
@@ -1,11 +1,10 @@
 package cn.luckydeer.spider;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@MapperScan(value = { "com.qufu.spider.dao" })
+//@MapperScan(value = { "com.qufu.spider.dao" })
 @EnableScheduling
 @SpringBootApplication
 public class SpiderApplication {
